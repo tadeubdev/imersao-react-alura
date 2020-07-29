de19e5c487fe08d5d41774596319d92b90f36004
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2020-07-29 18:57:35 -0300

    adiciona slider

--- src/pages/page-not-found/index.js
+++ src/pages/page-not-found/index.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import PageDefault from '../../components/PageDefault';
 
 function PageNotFound() {
   return (
-    <h2>Página não encontrada!</h2>
+    <PageDefault>
+
+      <h1>Ops!</h1>
+
+      <p>Página não encontrada!</p>
+
+      <br />
+
+      <Link to="/">
+        Ir para home
+      </Link>
+
+    </PageDefault>
   )
 }
 
