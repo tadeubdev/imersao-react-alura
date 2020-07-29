27ed5323d2b30456bc274f9867fd9c36843c8cec
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2020-07-29 09:57:06 -0300

    adiciona rotas

--- src/index.js
+++ src/index.js
@@ -3,9 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from 'react-router-dom'
+
+function CadastroVideo() {
+  return(
+    <div>
+      Página de cadastro de video
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/cadastro/video" component={CadastroVideo} />
+
+      <Route path="/" component={App} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
