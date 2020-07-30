3e2389c8460ac637bf22fcbbdeadd9cb5a6d72bf
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2020-07-30 10:06:04 -0300

    fix eslint

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Home from './pages/home';
-import PageNotFound from './pages/page-not-found'
-import CadastroDeVideo from './pages/cadastro-de-video'
-import CadastroDeCategoria from './pages/cadastro-de-categoria';
-
-import './reset.css';
-
 import {
   BrowserRouter,
   Switch,
-  Route
-} from 'react-router-dom'
+  Route,
+} from 'react-router-dom';
+
+import Home from './pages/home';
+import PageNotFound from './pages/page-not-found';
+import CadastroDeVideo from './pages/cadastro-de-video';
+import CadastroDeCategoria from './pages/cadastro-de-categoria';
+
+import './reset.css';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home} />
 
-      <Route path="/cadastro/video" exact component={CadastroDeVideo} />  
+      <Route path="/cadastro/video" exact component={CadastroDeVideo} />
 
-      <Route path="/cadastro/categoria" exact component={CadastroDeCategoria} />  
+      <Route path="/cadastro/categoria" exact component={CadastroDeCategoria} />
 
       <Route component={PageNotFound} />
     </Switch>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
