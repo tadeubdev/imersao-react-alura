27837058e6f379208e24dcecaeb36059c1bdcfdc
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2020-07-30 08:55:44 -0300

    adiciona estilizaçao aos formfields

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,18 +1,72 @@
 import React from 'react';
-import styled from 'styled-components';
-import './FormField.css';
+import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
 
-const Element = styled.input``;
+const FormFieldWrapper = styled.div`
+  position: relative;
+  textarea {
+    min-height: 150px;
+  }
+  input[type="color"] {
+    padding-left: 56px;
+  }
+`;
 
-function handleClick(event) {
-  if (event.currentTarget.querySelector('input')) {
-    event.currentTarget.querySelector('input').focus();
+const Label = styled.label``;
+
+Label.Text = styled.span`
+  color: #E5E5E5;
+  height: 57px;
+  position: absolute; 
+  top: 0;
+  left: 16px;
+  
+  display: flex;
+  align-items: center;
+  
+  transform-origin: 0% 0%;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 300;
+  
+  transition: .1s ease-in-out;
+`;
+
+const Input = styled.input`
+  background: #53585D;
+  color: #F5F5F5;
+  display: block;
+  width: 100%;
+  height: 57px;
+  font-size: 18px;
+
+  outline: 0;
+  border: 0;
+  border-top: 4px solid transparent;
+  border-bottom: 4px solid #53585D;
+
+  padding: 16px 16px;
+  margin-bottom: 45px;
+
+  resize: none;
+  border-radius: 4px;
+  transition: border-color .3s;
+
+  &:focus {
+    border-bottom-color: var(--primary);
   }
-  if (event.currentTarget.querySelector('textarea')) {
-    event.currentTarget.querySelector('textarea').focus();
+  &:focus:not([type='color']) + ${Label.Text} {
+    transform: scale(.6) translateY(-10px);
   }
-}
+  ${({ value }) => {
+    const hasValue = value.length > 0;
+    return hasValue && css`
+        &:not([type='color']) + ${Label.Text} {
+          transform: scale(.6) translateY(-10px);
+        }
+    `;
+  }}
+`;
 
 function FormField({
   as, label, type, name, value, onChange,
@@ -20,28 +74,25 @@ function FormField({
   const fieldId = `id_${name}`;
 
   return (
-    <div
-      role="button"
-      className="ElementContainer"
-      onClick={handleClick}
-      onKeyPress={handleClick}
-      tabIndex={0}
-    >
-      <label
+    <FormFieldWrapper>
+      <Label
         htmlFor={fieldId}
       >
-        {label}
-        :
-      </label>
+        <Input
+          as={as}
+          id={fieldId}
+          name={name}
+          type={type}
+          value={value}
+          onChange={onChange}
+        />
 
-      <Element
-        as={as}
-        name={name}
-        type={type}
-        value={value}
-        onChange={onChange}
-      />
-    </div>
+        <Label.Text>
+          {label}
+          :
+        </Label.Text>
+      </Label>
+    </FormFieldWrapper>
   );
 }
 
