070117bfef8dd3d58cd82ac88569f57bad95d2f1
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2020-08-01 08:37:53 -0300

    cria component de loading

---
+++ src/components/Loading/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <div id="loading">
+      <div className="lds-ring">
+        <div />
+        <div />
+        <div />
+        <div />
+      </div>
+    </div>
+  );
+}
